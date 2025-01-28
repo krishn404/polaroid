254bc2cd289b8a92698c08b6c225f41d84bbfafd
krishn404 <maharshikrishnakant@gmail.com>
2025-01-28 23:29:29 +0530

    PWA issue fix

--- next.config.mjs
+++ next.config.mjs
@@ -15,10 +15,12 @@ const nextConfig = {
 };
 
 const config = withPWA({
-  dest: 'public',
+  dest: '.next/static/worker',
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
+  buildExcludes: [/chunks\/.*$/],
+  publicExcludes: ['!sw.js', '!workbox-*.js'],
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/us-east-1\.tixte\.net\/uploads\/.*/i,
@@ -28,6 +30,35 @@ const config = withPWA({
         expiration: {
           maxEntries: 50,
           maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/_next\/image\?url=.+/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'next-images',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern: /\/_next\/static\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'static-resources',
+      },
+    },
+    {
+      urlPattern: /\.(?:eot|ttf|woff|woff2)$/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'static-fonts',
+        expiration: {
+          maxEntries: 20,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
         },
       },
     },
